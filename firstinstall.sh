8e7ad0bd59c4924237b952136620b1e27be496eb
Nebula <nebulalol@icloud.com>
2022-04-02 19:50:04 -0400

    Update firstinstall.sh

--- firstinstall.sh
+++ firstinstall.sh
@@ -7,32 +7,6 @@ apt update
 apt upgrade -y
 apt install sudo git curl wget -y
 
-read -r -p "Do you want to install Docker? [Y/n] " input
- 
-case $input in
-      [yY][eE][sS]|[yY])
-            curl https://get.docker.com | bash
-            ;;
-      [nN][oO]|[nN])
-            exit
-            ;;
-      *)
-            echo "Please answer Y or N."
-            exit 1
-            ;;
-esac
+[[ "$(read -e -p 'Do you want to install Docker? [y/N]> '; echo $REPLY)" == [Yy]* ]] && (curl https://get.docker.com | bash) || (echo Not installing Docker && exit)
 
-read -r -p "Do you want to install Docker Compose v2? [Y/n] " input2
- 
-case $input2 in
-      [yY][eE][sS]|[yY])
-            curl https://static.itsnebula.net/compose.sh | bash
-            ;;
-      [nN][oO]|[nN])
-            exit
-            ;;
-      *)
-            echo "Please answer Y or N."
-            exit 1
-            ;;
-esac
+[[ "$(read -e -p 'Do you want to install Docker Compose v2? [y/N]> '; echo $REPLY)" == [Yy]* ]] && (curl https://static.itsnebula.net/compose.sh | bash) || (echo Not installing Docker Compose && exit)
