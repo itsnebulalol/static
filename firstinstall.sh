78caba91d66ecc78e3b877ecf8024a88cdd34b62
Nebula <nebulalol@icloud.com>
2022-04-10 13:46:23 -0400

    Update firstinstall.sh

--- firstinstall.sh
+++ firstinstall.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-# Upgrades and installs nessasary tools
+# Upgrades and installs nessasary tools and installs docker
 # Run with "curl https://static.itsnebula.net/firstinstall.sh | bash"
-# THIS WILL INSTALL DOCKER - THE CHOICES ARE BROKEN
 
 apt update
 apt upgrade -y
 apt install sudo git curl wget -y
 
-echo "Do you want to install Docker? [y/N]> " 
-read docker
-if [[ $docker == y* ]]; then
-    curl https://get.docker.com | bash
-else
-    echo Not installing Docker
-    exit
-fi
-
-echo "Do you want to install Docker Compose v2? [y/N]> " 
-read dockerco
-if [[ $dockerco == y* ]]; then
-    curl https://static.itsnebula.net/compose.sh | bash
-else
-    echo Not installing Docker Compose
-    exit
-fi
+curl https://get.docker.com | bash
+curl https://static.itsnebula.net/compose.sh | bash
