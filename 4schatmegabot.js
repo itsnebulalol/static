f8bc2858b3c4bfd346fbf44d523cf5120b014aa5
Nebula <github@itsnebula.net>
2022-08-03 18:11:45 -0400

    Add files via upload

--- 4schatmegabot.js
+++ 4schatmegabot.js
@@ -10,7 +10,8 @@ msgBox[0].scrollTop = msgBox[0].scrollHeight;
 // Variables
 var botTitle =
     '<div><span class="user_name" style="color:#0000ff">MegaBot [BOT]</span> : <span class="user_message">';
-var lfmApiKey = "4a9f5581a9cdf20a699f540ac52a95c9";
+var lfmApiKey = "4a9f5581a9cdf20a699f540ac52a95c9"; // last.fm keys aren't even secret, and this isn't my key
+var urlRegex = /(https?:\/\/[^\s]+)/g;
 
 // Main bot loop
 setInterval(function () {
@@ -19,17 +20,47 @@ setInterval(function () {
         message.split(" : ")[1].split(">")[1].split("<")[0].toLowerCase()
     );
 
+    // Check for youtube links and give data about them
+    if (message.includes("youtube.com") || message.includes("youtu.be")) {
+        message.replace(urlRegex, function (url) {
+            id = url.split("//")[1].split("/")[1].replace("watch?v=", "");
+
+            $.getJSON(
+                `https://returnyoutubedislikeapi.com/votes?videoId=${id}`,
+                function (data) {
+                    msgBox.append(
+                        botTitle +
+                            "That's a cool YouTube link! Here's some stats about that video:</span></div>"
+                    );
+                    msgBox.append(
+                        `<div style="color:#000000">- ${data.likes} likes</div>`
+                    );
+                    msgBox.append(
+                        `<div style="color:#000000">- ${data.dislikes} dislikes</div>`
+                    );
+                    msgBox.append(
+                        `<div style="color:#000000">- ${data.views} views</div>`
+                    );
+                    msgBox[0].scrollTop = msgBox[0].scrollHeight;
+                }
+            );
+        });
+    }
+
     // Help command
     if (message == "!help") {
         msgBox.append(botTitle + "Here are my commands!" + "</span></div>");
         msgBox.append(
-            '<div style="color:#000000">  - !ticker <crypto> -- Get info for a cryptocurrency (eg. Bitcoin).</div>'
+            '<div style="color:#000000">- !ticker <crypto> | Get info for a cryptocurrency (eg. Bitcoin).</div>'
         );
         msgBox.append(
-            '<div style="color:#000000">  - !fact -- Get a random useless fact.</div>'
+            '<div style="color:#000000">- !fact | Get a random useless fact.</div>'
         );
         msgBox.append(
-            '<div style="color:#000000">  - !define <word(s)> -- Get a definition for a word(s) from Urban Dictionary.</div>'
+            '<div style="color:#000000">- !define <word(s)> | Get a definition for a word(s) from Urban Dictionary.</div>'
+        );
+        msgBox.append(
+            '<div style="color:#000000">- !lastfm <top|info> <username> | Get data from last.fm.</div>'
         );
         msgBox[0].scrollTop = msgBox[0].scrollHeight;
     }
@@ -90,28 +121,57 @@ setInterval(function () {
     if (message.startsWith("!lastfm top ")) {
         cmd = message.replace("!lastfm top ", "");
         $.getJSON(
-            `http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user=${cmd}&api_key=${lfmApiKey}&limit=10&format=json`,
+            `http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user=${cmd}&api_key=${lfmApiKey}&limit=5&format=json`,
             function (data) {
                 msgBox.append(
                     `${botTitle}${cmd}'s top 5 tracks are:</span></div>`
                 );
                 msgBox.append(
-                    `<div style="color:#000000">  1. ${data.toptracks.track[0].name} by ${data.toptracks.track[0].artist.name} (${data.toptracks.track[0].playcount} plays)</div>`
+                    `<div style="color:#000000">1. ${data.toptracks.track[0].name} by ${data.toptracks.track[0].artist.name} (${data.toptracks.track[0].playcount} plays)</div>`
                 );
                 msgBox.append(
-                    `<div style="color:#000000">  2. ${data.toptracks.track[1].name} by ${data.toptracks.track[1].artist.name} (${data.toptracks.track[1].playcount} plays)</div>`
+                    `<div style="color:#000000">2. ${data.toptracks.track[1].name} by ${data.toptracks.track[1].artist.name} (${data.toptracks.track[1].playcount} plays)</div>`
                 );
                 msgBox.append(
-                    `<div style="color:#000000">  3. ${data.toptracks.track[2].name} by ${data.toptracks.track[2].artist.name} (${data.toptracks.track[2].playcount} plays)</div>`
+                    `<div style="color:#000000">3. ${data.toptracks.track[2].name} by ${data.toptracks.track[2].artist.name} (${data.toptracks.track[2].playcount} plays)</div>`
                 );
                 msgBox.append(
-                    `<div style="color:#000000">  4. ${data.toptracks.track[3].name} by ${data.toptracks.track[3].artist.name} (${data.toptracks.track[3].playcount} plays)</div>`
+                    `<div style="color:#000000">4. ${data.toptracks.track[3].name} by ${data.toptracks.track[3].artist.name} (${data.toptracks.track[3].playcount} plays)</div>`
                 );
                 msgBox.append(
-                    `<div style="color:#000000">  5. ${data.toptracks.track[4].name} by ${data.toptracks.track[4].artist.name} (${data.toptracks.track[4].playcount} plays)</div>`
+                    `<div style="color:#000000">5. ${data.toptracks.track[4].name} by ${data.toptracks.track[4].artist.name} (${data.toptracks.track[4].playcount} plays)</div>`
                 );
                 msgBox[0].scrollTop = msgBox[0].scrollHeight;
             }
+        );
+    } else if (message.startsWith("!lastfm info ")) {
+        cmd = message.replace("!lastfm info ", "");
+        $.getJSON(
+            `http://ws.audioscrobbler.com/2.0/?method=user.getInfo&user=${cmd}&api_key=${lfmApiKey}&format=json`,
+            function (data) {
+                msgBox.append(`${botTitle}Info about ${cmd}:</span></div>`);
+                msgBox.append(
+                    `<div style="color:#000000">- ${data.user.playcount} tracks scrobbled.</div>`
+                );
+                msgBox.append(
+                    `<div style="color:#000000">- They're from the ${data.user.country}.</div>`
+                );
+                msgBox.append(
+                    `<div style="color:#000000">- They have ${data.user.playlists} playlists.</div>`
+                );
+                msgBox.append(
+                    `<div style="color:#000000">- See more <a href='${data.user.url}'>on their profile</a>.</div>`
+                );
+                msgBox[0].scrollTop = msgBox[0].scrollHeight;
+            }
+        );
+    } else if (message.startsWith("!lastfm ")) {
+        msgBox.append(
+            `${botTitle}Wrong usage! Correct usage: !lastfm <top> <username></span></div>`
+        );
+    } else if (message == "!lastfm") {
+        msgBox.append(
+            `${botTitle}Usage: !lastfm <top> <username></span></div>`
         );
     }
 }, 1000);
